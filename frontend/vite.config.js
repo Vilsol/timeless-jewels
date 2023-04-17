a1af9b074b137c388940d93a1d792ddb8d3f1356
Vilsol <me@vil.so>
2023-04-17 14:39:31 +0300

    fix: fix worker build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,23 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  // TODO: Remove once vite 4.3 is out
+  worker: {
+    plugins: [
+      {
+        name: 'remove-manifest',
+        configResolved(c) {
+          const manifestPlugin = c.worker.plugins.findIndex((p) => p.name === 'vite:manifest');
+          c.worker.plugins.splice(manifestPlugin, 1);
+          const ssrManifestPlugin = c.worker.plugins.findIndex(
+              (p) => p.name === 'vite:ssr-manifest'
+          );
+          c.plugins.splice(ssrManifestPlugin, 1);
+        }
+      }
+    ]
+  }
 };
 
 export default config;
