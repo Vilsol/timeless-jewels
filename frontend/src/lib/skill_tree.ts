8cce491f50c95e4531513c64e4e675026c3367b2
Vilsol <me@vil.so>
2024-03-30 23:53:29 +0200

    feat: upgrade assets to 3.24

--- frontend/src/lib/skill_tree.ts
+++ frontend/src/lib/skill_tree.ts
@@ -468,7 +468,7 @@ export const constructQuery = (jewel: number, conqueror: string, result: SearchW
 };
 
 export const openTrade = (jewel: number, conqueror: string, results: SearchWithSeed[]) => {
-  const url = new URL('https://www.pathofexile.com/trade/search/Affliction');
+  const url = new URL('https://www.pathofexile.com/trade/search/Necropolis');
   url.searchParams.set('q', JSON.stringify(constructQuery(jewel, conqueror, results)));
   window.open(url, '_blank');
 };
