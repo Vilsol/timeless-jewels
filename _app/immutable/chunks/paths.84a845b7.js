71465d366412abeb2ca89ea3052d7c33f68c86a6
Vilsol <Vilsol@users.noreply.github.com>
2025-07-02 11:53:30 +0000

    deploy: f3f1c316dcd081e77d63dc2fa514d5647dce1b90

--- _app/immutable/chunks/paths.84a845b7.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_10nztu8)==null?void 0:s.base)??"/timeless-jewels";var e;const a=((e=globalThis.__sveltekit_10nztu8)==null?void 0:e.assets)??t;export{a,t as b};
