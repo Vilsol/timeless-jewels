8305d2f365ae67a3d6dd6a7eb3a7f1cca1505b40
Ilya Starokozhov <grachara@gmail.com>
2026-05-06 04:05:43 +0300

    Updated openTrade in particular result

--- frontend/src/lib/components/SearchResult.svelte
+++ frontend/src/lib/components/SearchResult.svelte
@@ -35,7 +35,7 @@
     <div class="font-bold text-orange-500 text-center">
       Seed {set.seed} (weight {set.weight})
     </div>
-    <button class="px-3 bg-blue-500/40 rounded" on:click={() => openTrade(jewel, conqueror, [set], platform, league)}>Trade</button>
+    <button class="px-3 bg-blue-500/40 rounded" on:click={() => openTrade(jewel, conqueror, [set], platform, league, isLegacyTradersMode)}>Trade</button>
   </div>
   {#each set.skills as skill}
     <div class="mt-2">
